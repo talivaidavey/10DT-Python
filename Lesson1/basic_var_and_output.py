9c51128667f9286f0cbadf07418b51cfec97e544
talivaidavey <105258686+talivaidavey@users.noreply.github.com>
2022-08-18 10:10:06 +1200

    Add files via upload

---
+++ Lesson1/basic_var_and_output.py
@@ -0,0 +1,15 @@
+# D Talivai 
+# Variables 
+
+# Declaration of Variables
+name = Elephant
+numLegs = 4
+height = 5
+hasTail = True 
+
+# Output to Screen 
+print(name)
+print(numLegs)
+print(height)
+print(hasTail)
+
